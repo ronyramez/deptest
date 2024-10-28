f6b8e4b71aff210c191ebe6359ce4eb7cc2f9cb2
ronyramez <rony.ramez@hotmail.com>
2024-10-28 18:51:18 +0300

    minimal change

--- testdep/settings.py
+++ testdep/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9^85edyy%&skox^iogi%2urzio_-quap+b7itv6o(3o1%spzu9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
